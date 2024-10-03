8dc72f3a6f692479088797aefa0bb777ca14ab64
monsieuremre <e.paker@iat.rwth-aachen.de>
2024-10-03 16:02:47 +0200

    try link

--- src/helpers/edit2var.js
+++ src/helpers/edit2var.js
@@ -1,29 +1,11 @@
-// https://gitlab.com/fedora/docs/docs-website/ui-bundle
-
+/*
+https://gitlab.com/fedora/docs/docs-website/ui-bundle
+*/
 'use strict'
 
 module.exports = (editUrl, type) => {
   if (!editUrl || !type) return false
-  if (editUrl.includes('://github.com/')) {
-    if (type === 'issue') return editUrl.replace(/\/edit\/(\w+)\/(.*)$/, '/issues/new?title=[$1] Doc issue in file $2')
-    if (type === 'history') return editUrl.replace(/\/edit\//, '/commits/')
-  }
-  if (editUrl.includes('://pagure.io/')) {
-    if (type === 'issue') {
-      return editUrl.replace(/\/blob\/(\w+)\/f\/(.*)$/, '/new_issue?title=[$1] Doc issue in file $2')
-    }
-    if (type === 'history') {
-      const m = editUrl.match(/(.*)\/blob\/(\w+)\/f\/(.*)$/)
-      return `${m[1]}/history/${m[3]}?identifier=${m[2]}`
-    }
-  }
-  if (editUrl.includes('://gitlab.com/')) {
-    if (type === 'issue') {
-      return editUrl.replace(/\/edit\/(\w+)\/(.*)$/, '/issues/new?issue[title]=[$1] Doc issue in file $2')
-    }
-    if (type === 'history') return editUrl.replace(/\/edit\//, '/commits/')
-  }
-
+    if (type === 'issue') return editUrl.replace(/\/blob\/(\w+)\/(.*)$/, '/issues/new?title=[$1] Doc issue in file $2')
+    if (type === 'history') return editUrl.replace(/\/blob\//, '/commits/')
   return false
 }
-
